ead3d336b9ad79edbf87f907265c1ffbbc67067d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-11-23 08:52:13 +0000

    chore: Project init

---
+++ src/anibridge_providers/__init__.py
@@ -0,0 +1,5 @@
+"""Anibridge provider package.
+
+This package contains the interfaces for creating Anibridge providers. Actual concrete
+implementations of providers should reside in separate repositories/packages.
+"""
