147f6bbc4c65cdc259a62a9bb59d3a734dda67e1
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-11-23 08:53:56 +0000

    refactor: Rexport modules in package __init__

--- src/anibridge_providers/__init__.py
+++ src/anibridge_providers/__init__.py
@@ -3,3 +3,25 @@
 This package contains the interfaces for creating Anibridge providers. Actual concrete
 implementations of providers should reside in separate repositories/packages.
 """
+
+from anibridge_providers.library import LibraryProvider
+from anibridge_providers.list import ListProvider
+from anibridge_providers.provider import BaseProvider
+from anibridge_providers.registry import (
+    ProviderDescriptor,
+    ProviderKind,
+    get_global_registry,
+    register_library_provider,
+    register_list_provider,
+)
+
+__all__ = [
+    "BaseProvider",
+    "LibraryProvider",
+    "ListProvider",
+    "ProviderDescriptor",
+    "ProviderKind",
+    "get_global_registry",
+    "register_library_provider",
+    "register_list_provider",
+]
